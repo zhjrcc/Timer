5f0a78aef17fa640019a9e8c9c0b230f03d7d651
zhjrcc <zjrhello@gmail.com>
2024-09-17 00:58:05 +0800

    scaffold completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "./style.css"
+import TimerManager from "./TimerManager"
+
 function App() {
-  return (<>
-    
-  </>)
+  return (
+    <>
+      <main className="wrapper">
+        <h1 className="title">Countdown</h1>
+        <TimerManager />
+      </main>
+    </>
+  )
 }
 
 export default App
