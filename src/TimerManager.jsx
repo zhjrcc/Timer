f2d3a55d08c184accabd12a31eda8a766a21d6dd
zhjrcc <zjrhello@gmail.com>
2024-09-19 10:47:18 +0800

    multiple completed

--- src/TimerManager.jsx
+++ src/TimerManager.jsx
@@ -3,13 +3,30 @@ import Timer from "./Timer"
 import AddTimer from "./AddTimer"
 
 function TimerManager() {
-  const [startTime, setStartTime] = useState(0)
+  const [timers, setTimers] = useState([{ id: "first", startTime: 1 }])
+  const [isAdding, setAdding] = useState(false)
+
+  const onAdd = (startTime) => {
+    const id = `timer-${Math.random()}`
+    setTimers((prevState) => [...prevState, { id, startTime }])
+    setAdding(false)
+  }
+
+  const onDelete = (idToDelete) => {
+    setTimers((prevState) => prevState.filter(({ id }) => id !== idToDelete))
+  }
+
   return (
     <div className="timers">
-      {startTime > 0 ? (
-        <Timer startTime={startTime} onDelete={() => setStartTime(0)} />
+      {timers.map(({ id, startTime }) => (
+        <Timer startTime={startTime} key={id} id={id} onDelete={onDelete} />
+      ))}
+      {isAdding ? (
+        <AddTimer onAdd={onAdd} />
       ) : (
-        <AddTimer onAdd={setStartTime} />
+        <button className="timer timer-add" onClick={() => setAdding(true)}>
+          +
+        </button>
       )}
     </div>
   )
